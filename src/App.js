bbb52ae75e7263ce4391ae1fa95742261c38c03d
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-10 12:45:59 -0500

    Router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './containers/home';
+import SearchBar from './containers/search-bar';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        hey cara
-      </div>
+      <Router>
+        <div>
+          <SearchBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/containers/search-bar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        SearchBar Component
+        <input type="text" />
+      </div>
+    );
+  }
+}
