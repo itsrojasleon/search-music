9aaa11c5feead29d4e6b0cedd16bfb73253f627c
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-12 21:21:55 -0500

    grids fixed

--- src/components/button-view.js
+++ src/components/button-view.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ButtonView = ({ onToggle, toggleValue }) => {
   return (
-    <span>
-      <span style={{marginRight: 5}}>View: </span>
+    <span style={{textAlign: 'right', margin: '0 auto 20px auto', display: 'block'}}>
+      <span style={{marginRight: 5, color: '#727272'}}>View: </span>
       {toggleValue === false
         ? <i onClick={onToggle} className="fas fa-th"></i>
         : <i onClick={onToggle} className="fas fa-list-ul"></i>
--- src/components/track.js
+++ src/components/track.js
@@ -5,8 +5,8 @@ const Track = (props) => {
   const { customStyle } = props;
   return (
     <div className={customStyle === false ? 'track-box' : 'track-list'}>
-      <img width={customStyle === false ? '100%' : '20%' } src={`${props.album.images[0].url}`} alt={`${props.album.name}`} height="auto" />
-      <div>
+      <img style={{ borderRadius: 8 }} width={customStyle === false ? '100%' : '20%' } src={`${props.album.images[0].url}`} alt={`${props.album.name}`} height="auto" />
+      <div className={customStyle === false ? '' : 'description'}>
         <p>{props.name}</p>
         <p>{props.artists[0].name}</p>
       </div>
