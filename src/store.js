13c23fde7ff0621bc2ffa7447b2bea80fdb76acd
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-17 17:49:34 -0500

    redux logger disabled

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import reducer from './reducers';
 import throttle from 'lodash/throttle';
 import { loadState, saveState } from './localStorage';
@@ -10,7 +10,7 @@ const persistedState = loadState();
 const store = createStore(
   reducer,
   persistedState,
-  applyMiddleware(thunk, logger),
+  applyMiddleware(thunk),
 );
 
 store.subscribe(throttle(() => {
