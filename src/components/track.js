9a1ddab0b3a501c87391f6007004b1d99403da60
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-17 18:39:37 -0500

    card on hover show the icon

--- src/components/track.js
+++ src/components/track.js
@@ -6,6 +6,9 @@ import { selectSong } from '../actions/selectSong';
 import { toggleStart } from '../actions/controls';
 
 class Track extends React.Component {
+  state = {
+    hover: false,
+  }
   static propTypes = {
     customStyle: PropTypes.bool.isRequired,
     song: PropTypes.object.isRequired,
@@ -18,12 +21,16 @@ class Track extends React.Component {
   render() {
     const { customStyle, selectedSong, song, controls: { paused } } = this.props;
     return (
-      <div className={customStyle === false ? 'track-box' : 'track-list'}>
+      <div onMouseEnter={() => this.setState({ hover: true })} onMouseLeave={() => this.setState({ hover: false })} className={customStyle === false ? 'track-box' : 'track-list'}>
         <figure className="figure">
           <span className="artist-image-container">
             <img className="artist-image" width="100%" src={`${song.album.images[0].url}`} alt={`${song.album.name}`} height="auto" />
           </span>
-          <i onClick={this.handleSong} className={selectedSong.id === song.id && paused === false ? 'fas fa-pause play' : 'fas fa-play play'}></i>
+          {/*Here*/}
+          {this.state.hover === false
+            ? null
+            : <i onClick={this.handleSong} className={selectedSong.id === song.id && paused === false ? 'fas fa-pause play' : 'fas fa-play play'}></i>
+          }
         </figure>
         <div style={{ color: selectedSong.id === song.id ? 'rgb(222,0,62)' : '' }} className="description">
           <p>{song.name}</p>
