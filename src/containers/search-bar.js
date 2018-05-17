867f31d428e32205bdd0d629405ab05c94077a03
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-17 17:34:55 -0500

    adding new dependency called react-helmet

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -5,11 +5,15 @@ import { bindActionCreators } from 'redux';
 import { fetchSongs, emptySearch } from '../actions/songs';
 import _ from 'lodash';
 
+import Helmet from 'react-helmet';
+
 import SearchBarComponent from '../components/search-bar-component';
 
 class SearchBar extends Component {
+  state = { term: '' };
   handleChange = (e) => {
     const term = e.target.value;
+    this.setState({ term });
     const request = _.debounce(() => { this.props.fetchSongs(term) }, 1000);
     const redirect = _.debounce(() => { this.props.history.push(`/search-music/results/${term}`) }, 1000);
     request();
@@ -24,8 +28,15 @@ class SearchBar extends Component {
   }
   render() {
     const { toggle, theme } = this.props;
+    const { term } = this.state;
     return (
       <div>
+        <Helmet>
+          {term !== ''
+            ? <title>{term}</title>
+            : <title>Search Music</title>
+          }
+        </Helmet>
         <SearchBarComponent
           onChange={this.handleChange}
           onToggle={toggle}
