7336ca1d5764c23a24405c0bc6099accbc00a9f5
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-18 23:00:38 -0500

    home very ready

--- src/containers/home.js
+++ src/containers/home.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import lightWeb from '../utils/light-web.png';
 import lightMobile from '../utils/light-mobile.png';
-import DarkWeb from '../utils/dark-web.png';
-import DarkMobile from '../utils/dark-mobile.png';
+// import DarkWeb from '../utils/dark-web.png';
+// import DarkMobile from '../utils/dark-mobile.png';
 
 class Home extends Component {
   render() {
+    const { theme } = this.props;
     return (
       <div className="home">
-        <h1 className="title">Search Music</h1>
+        <h1 style={{ color: theme === false ? 'black' : 'white' }} className="title">Search Music</h1>
         <div className="image-container">
           <img className="light-web" src={lightWeb} />
           <img className="light-mobile" src={lightMobile} />
@@ -18,4 +19,7 @@ class Home extends Component {
     );
   }
 }
-export default connect()(Home);
+const mapStateToProps = ({ theme }) => ({
+  theme,
+});
+export default connect(mapStateToProps)(Home);
