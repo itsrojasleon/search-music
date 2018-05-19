769bc41b58784026e66ae6d433540e6c0e5068aa
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-05-18 22:53:54 -0500

    home ready

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import lightWeb from '../utils/light-web.png';
+import lightMobile from '../utils/light-mobile.png';
+import DarkWeb from '../utils/dark-web.png';
+import DarkMobile from '../utils/dark-mobile.png';
 
 class Home extends Component {
   render() {
     return (
-      <div>Home Component</div>
+      <div className="home">
+        <h1 className="title">Search Music</h1>
+        <div className="image-container">
+          <img className="light-web" src={lightWeb} />
+          <img className="light-mobile" src={lightMobile} />
+        </div>
+      </div>
     );
   }
 }
-export default Home;
+export default connect()(Home);
