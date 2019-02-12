ab646614563802b36d847dc520525d6481f5139c
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-11 21:42:27 -0600

    Enabling Google OAuth API

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'world' });
-});
-
-app.listen(5000, () => {
-  console.log('Listening...');
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
