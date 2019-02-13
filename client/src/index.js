d3df6215357b4ec6206664f7e1f5ddd2624bf353
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-13 14:12:45 -0600

    The auth reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
