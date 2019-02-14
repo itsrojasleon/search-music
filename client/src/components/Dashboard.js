34e40b6e88e2ac291465e3a56ad88a0a30b04876
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-13 18:14:12 -0600

    Login with Google

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-import Spinner from './Spinner';
 
 function Dashboard() {
   return (
     <div>
       <div>Dashboard Component</div>
-      <Spinner />
       <SearchBar />
     </div>
   );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,15 +5,20 @@ import { connect } from 'react-redux';
 function Header({ auth }) {
   return (
     <div className="ui pointing  menu">
-      <Link to="/" className="active item">
-        Home
+      <Link to="/" className="item">
+        <h2>SearchMusic</h2>
       </Link>
       <div className="right menu">
         {auth ? (
           <a href="/api/logout">Logout</a>
         ) : (
           <a href="/auth/google" className="ui item">
-            Login With Google
+            <div className="ui labeled button" tabIndex="0">
+              <span className="ui basic label">
+                <i className="google icon" />
+                Login with Google
+              </span>
+            </div>
           </a>
         )}
       </div>
