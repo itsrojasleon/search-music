2641ab22d1c78aa45067b96fd5929552a5fc4fdf
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-15 14:42:19 -0600

    box-shadow to header component

--- client/src/components/styled-components/header.js
+++ client/src/components/styled-components/header.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header`
   background-color: #fff;
-  box-shadow: 0 1px 12px 0 rgba(25, 25, 34, 0.12);
+  box-shadow: 0 5px 9px rgba(0, 0, 0, 0.05);
   left: 220px;
   min-width: 770px;
   padding: 0 24px;
