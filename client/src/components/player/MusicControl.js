f1d68a80ea53eb609bcfe1e02adc12d63e849186
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-03-02 13:28:31 -0600

    Align to center and right

--- client/src/components/player/MusicControl.js
+++ client/src/components/player/MusicControl.js
@@ -1,8 +1,7 @@
 import React from 'react';
+import { Container } from '../styled-components/player/music-control';
 
 function MusicControl() {
-  return (
-    <div>MusicControl</div>
-  );
+  return <Container>MusicControl</Container>;
 }
 export default MusicControl;
--- client/src/components/player/MusicVolume.js
+++ client/src/components/player/MusicVolume.js
@@ -1,8 +1,7 @@
 import React from 'react';
+import { Container } from '../styled-components/player/music-volume';
 
 function MusicVolume() {
-  return (
-    <div>MUSIC VOLUME</div>
-  );
+  return <Container>MUSIC VOLUME</Container>;
 }
 export default MusicVolume;
