a1cd1478adb4255b4cd208734527b9e1893c1c9f
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-03-03 16:47:17 -0600

    No console.log

--- client/src/components/player/MusicControl.js
+++ client/src/components/player/MusicControl.js
@@ -4,7 +4,7 @@ import { Container } from '../styled-components/player/music-control';
 function MusicControl(props) {
   const audioEl = React.useRef();
   console.log(audioEl);
-  const click = () => console.log(audioEl.current.play());
+  const click = () => audioEl.current.play();
   return (
     <Container onClick={click}>
       <audio ref={audioEl} controls src={props.track} />
