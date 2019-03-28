5c69b37c831fa58ee922e5f7b41549bd4cada466
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-03-28 14:42:25 -0600

    Format description

--- client/src/components/styled-components/player/data-song.js
+++ client/src/components/styled-components/player/data-song.js
@@ -18,12 +18,24 @@ export const Description = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 10px;
+  @media (max-width: 769px) {
+    margin-top: 15px;
+    margin-left: 0;
+    text-align: center;
+  }
 `;
 export const Name = styled.p`
   font-size: 15px;
+  @media (max-width: 769px) {
+    font-size: 18px;
+    margin-bottom: 3px;
+  }
 `;
 export const Artist = styled.p`
   font-size: 15px;
   color: rgb(140, 140, 148);
   font-size: 14px;
+  @media (max-width: 769px) {
+    font-size: 16px;
+  }
 `;
