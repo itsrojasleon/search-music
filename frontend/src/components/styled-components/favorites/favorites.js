36872de8344d0d56e9d8f327f38c13d92caaf189
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-04-23 20:13:01 -0500

    Switch to black

--- frontend/src/components/styled-components/favorites/favorites.js
+++ frontend/src/components/styled-components/favorites/favorites.js
@@ -4,22 +4,18 @@ export const Spinner = styled.div`
   font-size: 10px;
   margin: 50px auto;
   text-indent: -9999em;
-  width: 11em;
-  height: 11em;
+  width: 6em;
+  height: 6em;
   border-radius: 50%;
-  background: #ef9a9a;
-  background: linear-gradient(
-    to right,
-    #ef9a9a 10%,
-    rgba(255, 255, 255, 0) 42%
-  );
+  background: #000;
+  background: linear-gradient(to right, #000 10%, rgba(255, 255, 255, 0) 42%);
   position: relative;
   animation: spinner 1.4s infinite linear;
   transform: translateZ(0);
   &::before {
     width: 50%;
     height: 50%;
-    background: #ef9a9a;
+    background: #000;
     border-radius: 100% 0 0 0;
     position: absolute;
     top: 0;
