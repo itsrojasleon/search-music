3bc97ed5ca6455706d2e4364824bb3b49d7d8cd0
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-04-24 20:57:08 -0500

    A little bit of styles

--- frontend/src/components/favorites/FavoriteDetails.js
+++ frontend/src/components/favorites/FavoriteDetails.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import {
+  Container,
+  Titles,
+  Data
+} from '../styled-components/favorites/favoriteDetails';
 
 function FavoriteDetails(props) {
-  const { name } = props;
-  return <div>{name}</div>;
+  const { name, artists, album } = props;
+  return (
+    <Container>
+      <Titles>
+        <div>Track</div>
+        <div>Artist</div>
+        <div>Album</div>
+      </Titles>
+      <Data>
+        <div>{name}</div>
+        <div>{artists[0]['name']}</div>
+        <div>{album.name}</div>
+      </Data>
+    </Container>
+  );
 }
 export default FavoriteDetails;
--- frontend/src/components/styled-components/favorites/favoriteDetails.js
+++ frontend/src/components/styled-components/favorites/favoriteDetails.js
@@ -1,1 +1,23 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  background: white;
+  border: 1px solid rgb(230, 230, 230);
+  padding: 10px;
+`;
+const sameStyle = css`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  text-align: left;
+  border-bottom: 1px solid #ccc;
+  padding: 10px;
+`;
+export const Titles = styled.div`
+  ${sameStyle};
+`;
+export const Data = styled.div`
+  ${sameStyle};
+`;
