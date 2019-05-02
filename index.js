7fa6c1b246e369725332b47af6c1eb8af926889f
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-02 00:51:56 -0500

    update the path

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = require('./backend');
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('frontend/build'));
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
