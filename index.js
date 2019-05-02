67d0c4b3f0f8926081f787015c2fa1b7d6b6040d
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-02 01:57:30 -0500

    update homepage in package.json

--- index.js
+++ index.js
@@ -45,8 +45,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Listening...');
 });
-process.on('SIGTERM', () => {
-  server.close(() => {
-    console.log('Process terminated');
-  });
-});
