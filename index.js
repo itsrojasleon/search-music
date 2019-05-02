06a73412f058cb069301f1b7fa4b0a356f605842
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-02 14:29:32 -0500

    frontend to client

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ require('./routes/favoriteRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
 
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
