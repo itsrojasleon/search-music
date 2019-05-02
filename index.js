8e97c4690fafa8e4848b961ce9390e85349f94a2
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-02 02:18:26 -0500

    No

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname+'/frontend/build/index.html'));
   });
 }
 
